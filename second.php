db27b4e86e8a62e1781ff92213ecdf0435670555
ksenbelo <ksenbelo@greeny.cs.tlu.ee>
2016-09-14 14:50:52 +0300

    Sõnum muudatusest

---
+++ second.php
@@ -0,0 +1,32 @@
+<?php
+
+	$age = 5;
+	
+	if ($age < 18) {
+		
+		echo "alaealine";
+		
+	}
+	else {
+		
+		echo "täisealine";
+	
+	}
+		
+?>
+
+<br>
+
+<?php
+
+	//
+	for ($i = 0; $i < $age; $i = $i + 1) {
+		
+		echo "palju";
+		
+	}
+	
+	echo "õnne";
+
+
+?> 
